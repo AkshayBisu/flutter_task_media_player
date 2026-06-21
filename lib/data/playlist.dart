a077d814172186a9ff5927f3c2b650204457fa43
Akshay Bisu <akshay.bisu@crayoninfotech.com>
2026-06-21 18:30:38 +0530

    json data updated

--- lib/data/playlist.dart
+++ lib/data/playlist.dart
@@ -1,30 +1,21 @@
- 
 import '../models/media_item._model.dart';
+
 const Map<String, dynamic> playlistJson = {
   "result": [
     {
       "type": "image",
       "url":
-          "https://images.pexels.com/photos/32417863/pexels-photo-32417863.jpeg"
+          "https://images.pexels.com/photos/32417863/pexels-photo-32417863.jpeg",
     },
-    {
-      "type": "video",
-      "url": "https://samplelib.com/lib/preview/mp4/sample-20s.mp4"
-    },
-    {
-      "type": "video",
-      "url": "https://samplelib.com/lib/preview/mp4/sample-10s.mp4"
-    },
+    {"type": "video", "url": "https://samplelib.com/mp4/sample-5s-360p.mp4"},
+    {"type": "video", "url": "https://samplelib.com/mp4/sample-10s-360p.mp4"},
     {
       "type": "image",
       "url":
-          "https://images.pexels.com/photos/34213341/pexels-photo-34213341.jpeg"
+          "https://images.pexels.com/photos/34213341/pexels-photo-34213341.jpeg",
     },
-    {
-      "type": "video",
-      "url": "https://samplelib.com/lib/preview/mp4/sample-15s.mp4"
-    }
-  ]
+    {"type": "video", "url": "https://samplelib.com/mp4/sample-20s-360p.mp4"},
+  ],
 };
 
 List<MediaItem> loadPlaylist() {
